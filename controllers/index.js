71542d3b80891c250658cc5d02536487c7a6d638
Mia Ciasullo <miaciasullo@gmail.com>
2023-03-03 16:28:35 -0500

    moved session secret to environment variable

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const userRoutes = require('./userRoutes');
+const blogRoutes = require('./blogRoutes');
 
-router.use('/api', apiRoutes);
+router.use('/users', userRoutes);
+router.use('/blogs', blogRoutes);
 
 module.exports = router;
