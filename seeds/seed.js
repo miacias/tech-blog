cf5cca484224b96990328f77b76a8d9e012eb575
Mia Ciasullo <miaciasullo@gmail.com>
2023-03-06 00:26:01 -0500

    SOLVED: seed.js file can now add starter users, blogs, and comments to the database. added script "npm run seed" for easier re-seeding if needed. also added USE techblog_db; into the schema.sql file for ease of access when recreating DB

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,23 +1,33 @@
+// sequelize models
 const sequelize = require('../config/connection');
-const { User, Blog } = require('../models');
+const { User, Blog, Comment } = require('../models');
 
+// raw JSON starter data files
 const userData = require('./hashedUserData.json');
-// const blogData = require('./projectData.json');
+const blogData = require('./blogData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
+  // seeds users
   await sequelize.sync({ force: true });
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
-
-  // for (const blog of blogData) {
-  //   await Blog.create({
-  //     ...blog,
-  //     user_id: users[Math.floor(Math.random() * users.length)].id,
-  //   });
-  // }
-
+  // seeds blogs
+  const blogs = await Blog.bulkCreate(blogData, {
+    individualHooks: true,
+    returning: true,
+  });
+  // seeds comments randomly (on a random blog by a random user)
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      blog_id: blogs[Math.floor(Math.random() * blogs.length)].id,
+    });
+  };
+  // ends MySQL connection
   process.exit(0);
 };
 
