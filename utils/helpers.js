e49875ffc17df8068882ff02198155d1a7a21ad0
Mia Ciasullo <miaciasullo@gmail.com>
2023-03-09 15:51:18 -0500

    added comments to Handlebars helpers to make them more clear

--- utils/helpers.js
+++ utils/helpers.js
@@ -7,15 +7,15 @@ module.exports = {
         }).format(date);
         return timestamp;
     },
-    // verifies if blogReader is the same person as blogAuthor
+    // if reader is blog author, can edit/delete blog
     blogMatch: (blogReader, blogAuthor) => {
-        // console.log(blogReader, blogAuthor)
         if (blogReader === blogAuthor) {
             return true;
         } else {
             return false;
         }
     },
+    // if reader is comment author, can edit/delete comments
     commentMatch: (blogReader, commentAuthor) => {
         console.log(blogReader, commentAuthor)
         if (blogReader === commentAuthor) {
@@ -24,11 +24,20 @@ module.exports = {
             return false;
         }
     },
+    // if reader is blog author, can not add comments
     addCommentMatch: (blogReader, blogAuthor) => {
         if (blogReader !== blogAuthor) {
             return true;
         } else {
             return false;
+        }
+    },
+    // if no blogs, "consider writing first blog"
+    hasNoBlogs: (blogLength) => {
+        if (blogLength) {
+            return false;
+        } else {
+            return true;
         }
     }
 };
