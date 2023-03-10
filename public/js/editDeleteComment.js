db93ce87692ad4ce24815a729f1fdcf5a8388cb6
Mia Ciasullo <miaciasullo@gmail.com>
2023-03-10 15:37:33 -0500

    fixed script tag by moving functions into separate files: editDeleteBlog and editDeleteComment

---
+++ public/js/editDeleteComment.js
@@ -0,0 +1,30 @@
+// manage comments
+const deleteCommentBtn = document.querySelector('#delete-comment');
+const editCommentBtn = document.querySelector('#edit-comment');
+
+const deleteMyComment = async (event) => {
+    event.preventDefault();
+    // verifies user wants to delete
+    const areYouSure = confirm('Are you sure you want to delete this comment?');
+    if (areYouSure) {
+        const commentId = document.querySelector('.comment-post').id;
+        const username = document.querySelector('.user').id;
+        const response = await fetch(`/api/${username}/blogs/${blogId}`, {
+            method: 'DELETE',
+            headers: { 'content-type': 'application/json'}
+        });
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to delete blog');
+        }
+    }
+};
+
+const editMyComment = async () => {
+    console.log('hello edit')
+
+};
+
+deleteCommentBtn.addEventListener('click', deleteMyComment);
+editCommentBtn.addEventListener('click', editMyComment);
